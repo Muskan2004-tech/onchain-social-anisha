e09050c6178003465a06726e120d23d2f9d1211a
Anisha <anishajena73@gmail.com>
2025-07-07 06:33:56 +0000

    Initial working backend commit of OnChain Social DApp

--- src/onchain-social-frontend/src/App.jsx
+++ src/onchain-social-frontend/src/App.jsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import ProfilePage from "./pages/ProfilePage";
 
 function App() {
-return (
-<Router>
-<Routes>
-<Route path="/" element={<LandingPage />} />
-<Route path="/login" element={<LoginPage />} />
-<Route path="/register" element={<RegisterPage />} />
-<Route path="/profile" element={<ProfilePage />} />
-</Routes>
-</Router>
-);
+  return (
+    <div>
+      <h1>Welcome to Onchain Social!</h1>
+      <p>Frontend setup is working.</p>
+    </div>
+  );
 }
 
 export default App;
